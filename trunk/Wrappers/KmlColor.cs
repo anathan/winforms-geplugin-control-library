9899bacd3c748024175bd1ff59c453f3c3ee7c17
fraser.chapman@gmail.com <fraser.chapman@gmail.com@9f48ee18-d03e-11dd-9989-ad89728edd93>
2012-02-10 20:59:54 +0000

    Licence missing

--- trunk/Wrappers/KmlColor.cs
+++ trunk/Wrappers/KmlColor.cs
@@ -1,8 +1,26 @@
-﻿using System;
-using System.Drawing;
-
+ ﻿// <copyright file="KmlColor.cs" company="FC">
+// Copyright (c) 2012 Fraser Chapman
+// </copyright>
+// <author>Fraser Chapman</author>
+// <email>fraser.chapman@gmail.com</email>
+// <date>2012-03-10</date>
+// <summary>This file is part of FC.GEPluginCtrls
+// FC.GEPluginCtrls is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+// </summary>
 namespace FC.GEPluginCtrls
 {
+    using System;
+    using System.Drawing;
+
     /// <summary>
     /// Wrapper for the KmlColor com object, maps all the getter and setter methods to managed properties.
     /// The range of values for any one color component is 0 to 255 (0x00 to 0xff).
